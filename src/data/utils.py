1e722eb80b721582e731728623a896d89616cd65
PCSUPER <ale@ubuntu.ubuntu-domain>
2018-10-22 19:46:45 +0200

    modified:   notebooks/Labeling and MetaLabeling for Supervised Classification.ipynb 	modified:   notebooks/SampleWeights.ipynb 	modified:   notebooks/Tick, Volume, Dollar Volume Bars.ipynb 	modified:   src/data/utils.py 	modified:   src/features/bars.py

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+from numba import jit
+from tqdm import tqdm, tqdm_notebook
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import scipy.stats as stats
 
 def cprint(df):
     if not isinstance(df, pd.DataFrame):
@@ -18,4 +23,99 @@ def cprint(df):
     print()
 
 get_range = lambda df, col: (df[col].min(), df[col].max())
+
+@jit(nopython=True)
+def mad_outlier(y, thresh=3.):
+    '''
+    compute outliers based on mad
+    # args
+        y: assumed to be array with shape (N,1)
+        thresh: float()
+    # returns
+        array index of outliers
+    '''
+    median = np.median(y)
+    diff = np.sum((y - median)**2, axis=-1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > thresh
+
+
+def select_sample_data(ref, sub, price_col, date):
+    '''
+    select a sample of data based on date, assumes datetimeindex
+    
+    # args
+        ref: pd.DataFrame containing all ticks
+        sub: subordinated pd.DataFrame of prices
+        price_col: str(), price column
+        date: str(), date to select
+    # returns
+        xdf: ref pd.Series
+        xtdf: subordinated pd.Series
+    '''
+    xdf = ref[price_col].loc[date]
+    xtdf = sub[price_col].loc[date]
+    return xdf, xtdf
+
+def plot_sample_data(ref, sub, bar_type, *args, **kwds):
+    f,axes=plt.subplots(3,sharex=True, sharey=True, figsize=(10,7))
+    ref.plot(*args, **kwds, ax=axes[0], label='price')
+    sub.plot(*args, **kwds, ax=axes[0], marker='X', ls='', label=bar_type)
+    axes[0].legend();
+    
+    ref.plot(*args, **kwds, ax=axes[1], label='price', marker='o')
+    sub.plot(*args, **kwds, ax=axes[2], ls='', marker='X',
+             color='r', label=bar_type)
+
+    for ax in axes[1:]: ax.legend()
+    plt.tight_layout()
+    
+    return
+
+def plot_autocorr(bar_types,bar_returns):
+    f,axes=plt.subplots(len(bar_types),figsize=(10,7), sharey=True)
+
+    for i, (bar, typ) in enumerate(zip(bar_returns, bar_types)):
+        sm.graphics.tsa.plot_acf(bar, lags=120, ax=axes[i],
+                                 alpha=0.05, unbiased=True, fft=True,
+                                 zero=False,
+                                 title=f'{typ} AutoCorr')
+    plt.tight_layout()   
+    
+def plot_hist(bar_types,bar_rets):
+    f,axes=plt.subplots(len(bar_types),figsize=(10,6))
+    for i, (bar, typ) in enumerate(zip(bar_returns, bar_types)):
+        g = sns.distplot(bar, ax=axes[i], kde=False, label=typ)
+        g.set(yscale='log')
+        axes[i].legend()
+    plt.tight_layout()
+
+def jb(x,test=True): 
+    '''
+    the lower the best
+    '''
+    np.random.seed(12345678)
+    if test: return stats.jarque_bera(x)[0]
+    return stats.jarque_bera(x)[1]
+
+def shapiro(x,test=True): 
+    '''
+    The greater the better    
+    '''
+    np.random.seed(12345678)
+    if test: return stats.shapiro(x)[0]
+    return stats.shapiro(x)[1]
+
+def get_test_stats(bar_types,bar_returns,test_func,*args,**kwds):
+    
+    dct = {bar:(int(bar_ret.shape[0]), test_func(bar_ret,*args,**kwds)) 
+           for bar,bar_ret in zip(bar_types,bar_returns)}
+    df = (pd.DataFrame.from_dict(dct)
+          .rename(index={0:'sample_size',1:f'{test_func.__name__}_stat'})
+          .T)
+    return df
 
--- src/features/bars.py
+++ src/features/bars.py
@@ -7,8 +7,15 @@ from tqdm import tqdm
 def returns(s):
     arr = np.diff(np.log(s))
     return (pd.Series(arr, index=s.index[1:]))
+
+def count_bars(df, price_col='price', freq='1W'):
+    return df.groupby(pd.TimeGrouper(freq))[price_col].count()
+
+def scale(s):
+    return (s-s.min())/(s.max()-s.min())
+
 #========================================================
-def tick_bars(df, column, m):
+def tick_bars(df, price_column, m):
     '''
     compute tick bars
 
@@ -19,33 +26,53 @@ def tick_bars(df, column, m):
     # returns
         idx: list of indices
     '''
-    t = df[column]
+    t = df[price_column]
     ts = 0
     idx = []
     for i, x in enumerate(tqdm(t)):
-        ts += 1 
+        ts += 1
         if ts >= m:
             idx.append(i)
             ts = 0
             continue
     return idx
 
-def tick_bar_df(df, column, m):
-    idx = tick_bars(df, column, m)
-    return df.iloc[idx]
-#========================================================
-def volume_bars(df, column, m):
+def tick_bar_df(df, price_column, m):
+    idx = tick_bars(df, price_column, m)
+    return df.iloc[idx].drop_duplicates()
+
+def get_ohlc(ref, sub):
+    '''
+    fn: get ohlc from custom bars
+    
+    # args
+        ref : reference pandas series with all prices
+        sub : custom tick pandas series
+    # returns
+        tick_df : dataframe with ohlc values
+    '''
+    ohlc = []
+    for i in tqdm(range(sub.index.shape[0]-1)):
+        start,end = sub.index[i], sub.index[i+1]
+        tmp_ref = ref.loc[start:end]
+        max_px, min_px = tmp_ref.max(), tmp_ref.min()
+        o,h,l,c = sub.iloc[i], max_px, min_px, sub.iloc[i+1]
+        ohlc.append((end,start,o,h,l,c))
+    cols = ['end','start','open','high','low','close']
+    return (pd.DataFrame(ohlc,columns=cols))
+
+def volume_bars(df, volume_column, m):
     '''
     compute volume bars
 
     # args
         df: pd.DataFrame()
-        column: name for price data
+        volume_column: name for volume data
         m: int(), threshold value for volume
     # returns
         idx: list of indices
     '''
-    t = df[column]
+    t = df[volume_column]
     ts = 0
     idx = []
     for i, x in enumerate(tqdm(t)):
@@ -56,22 +83,22 @@ def volume_bars(df, column, m):
             continue
     return idx
 
-def volume_bar_df(df, column, m):
-    idx = volume_bars(df, column, m)
-    return df.iloc[idx]
-#========================================================
-def dollar_bars(df, column, m):
+def volume_bar_df(df, volume_column, m):
+    idx = volume_bars(df, volume_column, m)
+    return df.iloc[idx].drop_duplicates()
+
+def dollar_bars(df, dv_column, m):
     '''
     compute dollar bars
 
     # args
         df: pd.DataFrame()
-        column: name for price data
+        dv_column: name for dollar volume data
         m: int(), threshold value for dollars
     # returns
         idx: list of indices
     '''
-    t = df[column]
+    t = df[dv_column]
     ts = 0
     idx = []
     for i, x in enumerate(tqdm(t)):
@@ -82,10 +109,39 @@ def dollar_bars(df, column, m):
             continue
     return idx
 
-def dollar_bar_df(df, column, m):
-    idx = dollar_bars(df, column, m)
-    return df.iloc[idx]
+def dollar_bar_df(df, dv_column, m):
+    idx = dollar_bars(df, dv_column, m)
+    return df.iloc[idx].drop_duplicates()
 #========================================================
+
+
+def test_t_abs(absTheta,t,E_bs):
+    """
+    Bool function to test inequality
+    *row is assumed to come from df.itertuples()
+    -absTheta: float(), row.absTheta
+    -t: pd.Timestamp()
+    -E_bs: float(), row.E_bs
+    """
+    return (absTheta >= t*E_bs)
+
+def agg_imbalance_bars(df):
+    """
+    Implements the accumulation logic
+    """
+    start = df.index[0]  
+    bars = []
+    for row in tqdm(df.itertuples()):
+        t_abs = row.absTheta
+        rowIdx = row.Index
+        E_bs = row.E_bs
+        
+        t = df.loc[start:rowIdx].shape[0]
+        if t<1: t=1 # if t lt 1 set equal to 1
+        if test_t_abs(t_abs,t,E_bs):
+            bars.append((start,rowIdx,t))
+            start = rowIdx
+    return bars
 
 @jit(nopython=True)
 def numba_isclose(a,b,rel_tol=1e-09,abs_tol=0.0):
