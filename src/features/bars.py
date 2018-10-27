6740435e0e9c283c09d76b12279facd710bf1ba5
PCSUPER <ale@ubuntu.ubuntu-domain>
2018-10-27 14:50:38 +0200

    Aligned with main repo 	modified:   README.md 	modified:   notebooks/04. Sample Weights.ipynb 	modified:   notebooks/05. Fractionally Differentiated Features.ipynb 	modified:   notebooks/Labeling and MetaLabeling for Supervised Classification.ipynb 	modified:   notebooks/Tick, Volume, Dollar Volume Bars.ipynb 	modified:   requirements.txt 	modified:   setup.py 	new file:   src/CONSTANTS.py 	modified:   src/features/bars.py 	new file:   src/features/snippets.py 	new file:   src/utils/__init__.py 	new file:   src/utils/utils.py

--- src/features/bars.py
+++ src/features/bars.py
@@ -7,13 +7,6 @@ from tqdm import tqdm
 def returns(s):
     arr = np.diff(np.log(s))
     return (pd.Series(arr, index=s.index[1:]))
-
-def count_bars(df, price_col='price', freq='1W'):
-    return df.groupby(pd.TimeGrouper(freq))[price_col].count()
-
-def scale(s):
-    return (s-s.min())/(s.max()-s.min())
-
 #========================================================
 def tick_bars(df, price_column, m):
     '''
@@ -39,35 +32,15 @@ def tick_bars(df, price_column, m):
 
 def tick_bar_df(df, price_column, m):
     idx = tick_bars(df, price_column, m)
-    return df.iloc[idx].drop_duplicates()
-
-def get_ohlc(ref, sub):
-    '''
-    fn: get ohlc from custom bars
-    
-    # args
-        ref : reference pandas series with all prices
-        sub : custom tick pandas series
-    # returns
-        tick_df : dataframe with ohlc values
-    '''
-    ohlc = []
-    for i in tqdm(range(sub.index.shape[0]-1)):
-        start,end = sub.index[i], sub.index[i+1]
-        tmp_ref = ref.loc[start:end]
-        max_px, min_px = tmp_ref.max(), tmp_ref.min()
-        o,h,l,c = sub.iloc[i], max_px, min_px, sub.iloc[i+1]
-        ohlc.append((end,start,o,h,l,c))
-    cols = ['end','start','open','high','low','close']
-    return (pd.DataFrame(ohlc,columns=cols))
-
+    return df.iloc[idx]
+#========================================================
 def volume_bars(df, volume_column, m):
     '''
     compute volume bars
 
     # args
         df: pd.DataFrame()
-        volume_column: name for volume data
+        column: name for volume data
         m: int(), threshold value for volume
     # returns
         idx: list of indices
@@ -85,15 +58,15 @@ def volume_bars(df, volume_column, m):
 
 def volume_bar_df(df, volume_column, m):
     idx = volume_bars(df, volume_column, m)
-    return df.iloc[idx].drop_duplicates()
-
+    return df.iloc[idx]
+#========================================================
 def dollar_bars(df, dv_column, m):
     '''
     compute dollar bars
 
     # args
         df: pd.DataFrame()
-        dv_column: name for dollar volume data
+        column: name for dollar volume data
         m: int(), threshold value for dollars
     # returns
         idx: list of indices
@@ -111,37 +84,8 @@ def dollar_bars(df, dv_column, m):
 
 def dollar_bar_df(df, dv_column, m):
     idx = dollar_bars(df, dv_column, m)
-    return df.iloc[idx].drop_duplicates()
+    return df.iloc[idx]
 #========================================================
-
-
-def test_t_abs(absTheta,t,E_bs):
-    """
-    Bool function to test inequality
-    *row is assumed to come from df.itertuples()
-    -absTheta: float(), row.absTheta
-    -t: pd.Timestamp()
-    -E_bs: float(), row.E_bs
-    """
-    return (absTheta >= t*E_bs)
-
-def agg_imbalance_bars(df):
-    """
-    Implements the accumulation logic
-    """
-    start = df.index[0]  
-    bars = []
-    for row in tqdm(df.itertuples()):
-        t_abs = row.absTheta
-        rowIdx = row.Index
-        E_bs = row.E_bs
-        
-        t = df.loc[start:rowIdx].shape[0]
-        if t<1: t=1 # if t lt 1 set equal to 1
-        if test_t_abs(t_abs,t,E_bs):
-            bars.append((start,rowIdx,t))
-            start = rowIdx
-    return bars
 
 @jit(nopython=True)
 def numba_isclose(a,b,rel_tol=1e-09,abs_tol=0.0):
@@ -164,397 +108,3 @@ def get_imbalance(t):
         t_bt = bt(t[i-1], t[i], bs[:i-1])
         bs[i-1] = t_bt
     return bs[:-1] # remove last value
-
-def get_imbalanced_df(df, price_column='price', imbalanced_column='dv', wsize=1000):
-    
-    tidx = get_imbalance(df[price_column].values)*df[imbalanced_column].iloc[1:]
-    
-    wndo = tidx.shape[0]//wsize
-    ## Expected value of bs approximated by ewm
-    E_bs = tidx.ewm(wndo).mean() # expected `bs`
-    E_T = pd.Series(range(tidx.shape[0]), index=tidx.index).ewm(wndo).mean()
-    df0 =(pd.DataFrame().assign(bs=tidx)
-      .assign(E_T=E_T).assign(E_bs=E_bs)
-      .assign(absMul=lambda df: df.E_T*np.abs(df.E_bs))
-      .assign(absTheta=tidx.cumsum().abs()))
-        
-    bars = agg_imbalance_bars(df0)
-    test_imb_bars = (pd.DataFrame(bars,columns=['start','stop','Ts'])
-                 .drop_duplicates())
-    
-    test_imb_bars.set_index('stop')['Ts'].plot()
-
-    imb_bars = df.loc[test_imb_bars.stop].drop_duplicates()
-    return imb_bars, test_imb_bars
-
-# ======================================
-def getTEvents(gRaw, h):
-    tEvents, sPos, sNeg = [], 0, 0
-    diff = np.log(gRaw).diff().dropna()#.abs()
-    for i in tqdm(diff.index[1:]):
-        try:
-            pos, neg = float(sPos+diff.loc[i]), float(sNeg+diff.loc[i])
-        except Exception as e:
-            print(e)
-            print(sPos+diff.loc[i], type(sPos+diff.loc[i]))
-            print(sNeg+diff.loc[i], type(sNeg+diff.loc[i]))
-            break
-        sPos, sNeg=max(0., pos), min(0., neg)
-        if sNeg<-h:
-            sNeg=0;tEvents.append(i)
-        elif sPos>h:
-            sPos=0;tEvents.append(i)
-    return pd.DatetimeIndex(tEvents)
-
-
-def getDailyVol(close,span0=100):
-    # daily vol reindexed to close
-    df0=close.index.searchsorted(close.index-pd.Timedelta(days=1))
-    df0=df0[df0>0]   
-    df0=(pd.Series(close.index[df0-1], 
-                   index=close.index[close.shape[0]-df0.shape[0]:]))   
-    try:
-        df0=close.loc[df0.index]/close.loc[df0.values].values-1 # daily rets
-    except Exception as e:
-        print(f'error: {e}\nplease confirm no duplicate indices')
-    df0=df0.ewm(span=span0).std().rename('dailyVol')
-    return df0
-
-def applyPtSlOnT1(close,events,ptSl,molecule):
-    # apply stop loss/profit taking, if it takes place before t1 (end of event)
-    events_=events.loc[molecule]
-    out=events_[['t1']].copy(deep=True)
-    if ptSl[0]>0: pt=ptSl[0]*events_['trgt']
-    else: pt=pd.Series(index=events.index) # NaNs
-    if ptSl[1]>0: sl=-ptSl[1]*events_['trgt']
-    else: sl=pd.Series(index=events.index) # NaNs
-    for loc,t1 in events_['t1'].fillna(close.index[-1]).iteritems():
-        df0=close[loc:t1] # path prices
-        df0=(df0/close[loc]-1)*events_.at[loc,'side'] # path returns
-        out.loc[loc,'sl']=df0[df0<sl[loc]].index.min() # earliest stop loss
-        out.loc[loc,'pt']=df0[df0>pt[loc]].index.min() # earliest profit taking
-    return out
-
-#Calcola, in base ai parametri passati, il target e la data in cui viene raggiunto
-def getEvents(close, tEvents, ptSl, trgt, minRet, numThreads, t1=False, side=None):
-    #1) get target
-    trgt=trgt.loc[tEvents]
-    trgt=trgt[trgt>minRet] # minRet
-    #2) get t1 (max holding period)
-    if t1 is False:
-        t1=pd.Series(pd.NaT, index=tEvents)
-    #3) form events object, apply stop loss on t1
-    if side is None:
-        side_,ptSl_= pd.Series(1.,index=trgt.index), [ptSl[0], ptSl[0]]
-    else: 
-        side_,ptSl_= side.loc[trgt.index], ptSl[:2]
-
-    events=(pd.concat({'t1':t1,'trgt':trgt,'side':side_}, axis=1)
-            .dropna(subset=['trgt']))
-    
-    df0=mpPandasObj(func=applyPtSlOnT1,pdObj=('molecule',events.index),
-                    numThreads=numThreads,close=close,events=events,
-                    ptSl=ptSl_)
-    
-    events['t1']=df0.dropna(how='all').min(axis=1) # pd.min ignores nan
-    
-    if side is None:
-        events=events.drop('side',axis=1)
-    
-    return events
-
-def addVerticalBarrier(tEvents, close, numDays=1):
-    t1=close.index.searchsorted(tEvents+pd.Timedelta(days=numDays))
-    t1=t1[t1<close.shape[0]]
-    t1=(pd.Series(close.index[t1],index=tEvents[:t1.shape[0]]))
-    return t1
-
-# In base alla data di inizio e di raggiungimento del target determina il ritorno in termini
-# percentuali e assegna l'etichetta
-def getBins(events, close):
-    '''
-    Compute event's outcome (including side information, if provided).
-    events is a DataFrame where:
-    -events.index is event's starttime
-    -events['t1'] is event's endtime
-    -events['trgt'] is event's target
-    -events['side'] (optional) implies the algo's position side
-    Case 1: ('side' not in events): bin in (-1,1) <-label by price action
-    Case 2: ('side' in events): bin in (0,1) <-label by pnl (meta-labeling)
-    '''
-    #1) prices aligned with events
-    events_=events.dropna(subset=['t1'])
-    px=events_.index.union(events_['t1'].values).drop_duplicates()
-    px=close.reindex(px,method='bfill')
-    #2) create out object
-    out=pd.DataFrame(index=events_.index)
-    out['ret']=px.loc[events_['t1'].values].values/px.loc[events_.index]-1
-    if 'side' in events_:out['ret']*=events_['side'] # meta-labeling
-    out['bin']=np.sign(out['ret'])
-    if 'side' in events_:out.loc[out['ret']<=0,'bin']=0 # meta-labeling
-    return out
-
-
-def dropLabels(events, minPct=.05):
-    # apply weights, drop labels with insufficient examples
-    while True:
-        df0=events['bin'].value_counts(normalize=True)
-        if df0.min()>minPct or df0.shape[0]<3:break
-        print('dropped label: ', df0.argmin(),df0.min())
-        events=events[events['bin']!=df0.argmin()]
-    return events
-
-#________________________________
-def reportProgress(jobNum,numJobs,time0,task):
-    # Report progress as asynch jobs are completed
-    msg=[float(jobNum)/numJobs, (time.time()-time0)/60.]
-    msg.append(msg[1]*(1/msg[0]-1))
-    timeStamp=str(dt.datetime.fromtimestamp(time.time()))
-    msg=timeStamp+' '+str(round(msg[0]*100,2))+'% '+task+' done after '+ \
-        str(round(msg[1],2))+' minutes. Remaining '+str(round(msg[2],2))+' minutes.'
-    if jobNum<numJobs:sys.stderr.write(msg+'\r')
-    else:sys.stderr.write(msg+'\n')
-    return
-#________________________________
-def processJobs(jobs,task=None,numThreads=24):
-    # Run in parallel.
-    # jobs must contain a 'func' callback, for expandCall
-    if task is None:task=jobs[0]['func'].__name__
-    pool=mp.Pool(processes=numThreads)
-    outputs,out,time0=pool.imap_unordered(expandCall,jobs),[],time.time()
-    # Process asyn output, report progress
-    for i,out_ in enumerate(outputs,1):
-        out.append(out_)
-        reportProgress(i,len(jobs),time0,task)
-    pool.close();pool.join() # this is needed to prevent memory leaks
-    return out
-
-def expandCall(kargs):
-    # Expand the arguments of a callback function, kargs['func']
-    func=kargs['func']
-    del kargs['func']
-    out=func(**kargs)
-    return out
-
-def _pickle_method(method):
-    func_name=method.im_func.__name__
-    obj=method.im_self
-    cls=method.im_class
-    return _unpickle_method, (func_name,obj,cls)
-#________________________________
-def _unpickle_method(func_name,obj,cls):
-    for cls in cls.mro():
-        try:func=cls.__dict__[func_name]
-        except KeyError:pass
-        else:break
-    return func.__get__(obj,cls)
-#________________________________
-import copyreg,types, multiprocessing as mp
-copyreg.pickle(types.MethodType,_pickle_method,_unpickle_method)
-
-
-
-
-def linParts(numAtoms,numThreads):
-    # partition of atoms with a single loop
-    parts=np.linspace(0,numAtoms,min(numThreads,numAtoms)+1)
-    parts=np.ceil(parts).astype(int)
-    return parts
-
-def nestedParts(numAtoms,numThreads,upperTriang=False):
-    # partition of atoms with an inner loop
-    parts,numThreads_=[0],min(numThreads,numAtoms)
-    for num in range(numThreads_):
-        part=1+4*(parts[-1]**2+parts[-1]+numAtoms*(numAtoms+1.)/numThreads_)
-        part=(-1+part**.5)/2.
-        parts.append(part)
-    parts=np.round(parts).astype(int)
-    if upperTriang: # the first rows are heaviest
-        parts=np.cumsum(np.diff(parts)[::-1])
-        parts=np.append(np.array([0]),parts)
-    return parts
-
-def mpPandasObj(func,pdObj,numThreads=24,mpBatches=1,linMols=True,**kargs):
-    '''
-    Parallelize jobs, return a dataframe or series
-    + func: function to be parallelized. Returns a DataFrame
-    + pdObj[0]: Name of argument used to pass the molecule
-    + pdObj[1]: List of atoms that will be grouped into molecules
-    + kwds: any other argument needed by func
-    
-    Example: df1=mpPandasObj(func,('molecule',df0.index),24,**kwds)
-    '''
-    #if linMols:parts=linParts(len(argList[1]),numThreads*mpBatches)
-    #else:parts=nestedParts(len(argList[1]),numThreads*mpBatches)
-    if linMols:parts=linParts(len(pdObj[1]),numThreads*mpBatches)
-    else:parts=nestedParts(len(pdObj[1]),numThreads*mpBatches)
-    
-    jobs=[]
-    for i in range(1,len(parts)):
-        job={pdObj[0]:pdObj[1][parts[i-1]:parts[i]],'func':func}
-        job.update(kargs)
-        jobs.append(job)
-    if numThreads==1:out=processJobs_(jobs)
-    else: out=processJobs(jobs,numThreads=numThreads)
-    if isinstance(out[0],pd.DataFrame):df0=pd.DataFrame()
-    elif isinstance(out[0],pd.Series):df0=pd.Series()
-    else:return out
-    for i in out:df0=df0.append(i)
-    df0=df0.sort_index()
-    return df0
-
-def processJobs_(jobs):
-    # Run jobs sequentially, for debugging
-    out=[]
-    for job in jobs:
-        out_=expandCall(job)
-        out.append(out_)
-    return out
-
-import multiprocessing as mp
-import datetime as dt
-
-
-def bbands(price, window=None, width=None, numsd=None):
-    """ returns average, upper band, and lower band"""
-    ave = price.rolling(window).mean()
-    sd = price.rolling(window).std(ddof=0)
-    if width:
-        upband = ave * (1+width)
-        dnband = ave * (1-width)
-        return price, np.round(ave,3), np.round(upband,3), np.round(dnband,3)        
-    if numsd:
-        upband = ave + (sd*numsd)
-        dnband = ave - (sd*numsd)
-        return price, np.round(ave,3), np.round(upband,3), np.round(dnband,3)
-def get_up_cross(df, col):
-    # col is price column
-    crit1 = df[col].shift(1) < df.upper.shift(1)  
-    crit2 = df[col] > df.upper
-    return df[col][(crit1) & (crit2)]
-
-def get_down_cross(df, col):
-    # col is price column    
-    crit1 = df[col].shift(1) > df.lower.shift(1) 
-    crit2 = df[col] < df.lower
-    return df[col][(crit1) & (crit2)]
-
-def mpNumCoEvents(closeIdx, t1, molecule):
-    '''
-    Compute the number of concurrent events per bar
-    +molecule[0] is the date of the first event on which the weight will be computed
-    +molecule[-1] is the date of the last event on which the weight will be computed
-    Any event that starts before t1[molecule].max() impacts the count
-    '''
-    #1) Find any events that span the period molecule[0] molecule[-1]
-    t1 = t1.fillna(closeIdx[-1])
-    t1 = t1[t1 >= molecule[0]]
-    t1 = t1.loc[:t1[molecule].max()]
-    #2) Count events spanning the bar
-    iloc = closeIdx.searchsorted(np.array([t1.index[0], t1.max()]))
-    count = pd.Series(0, index=closeIdx[iloc[0]:iloc[-1]+1])
-    for tIn, tOut in t1.iteritems():
-        count.loc[tIn:tOut] += 1
-    return count.loc[molecule[0]:t1[molecule].max()]
-
-
-def getWeights(d, size):
-    # Threshold  > 0
-    w=[1.]
-    for k in range(1, size):
-        w_ = -w[-1]/k*(d-k+1)
-        w.append(w_)
-    w = np.array(w[::-1]).reshape(-1, 1)
-    return w
-
-# ------------------------
-def plotWeights(dRange, nPlots, size):
-    w = pd.DataFrame()
-    for d in np.linspace(dRange[0], dRange[1], nPlots):
-        w_ = getWeights(d, size=size)
-        w_ = pd.DataFrame(w_, index=range(w_.shape[0])[::-1], columns=[d])
-        w = w.join(w_, how='outer')
-    ax = w.plot(figsize=(12, 8))
-    ax.legend(loc='lower right')
-    plt.show()
-    return
-
-def mpSampleTW(t1, numCoEvents, molecule):
-    # Derive average uniqueness over the event's lifespan
-    wght = pd.Series(index=molecule)
-    for tIn, tOut in t1.loc[wght.index].iteritems():
-        wght.loc[tIn] = (1./numCoEvents.loc[tIn:tOut]).mean()
-    return wght
-
-def acf(x, length=20):
-    return np.array([1]+[np.corrcoef(x[:-i], x[i:])[0,1] for i in range(1, length)])
-
-def fractDiff(series, d, thres=0.1):
-    '''
-    Increasing width window, with treatment of NaNs
-    Note 1. for thres = 1, nothing is skipped
-    Note 2. d can be any positive frational, not necesserly bounded in [0, 1]
-    '''
-    #1) Compute weights for the longest series
-    w = getWeights(d, series.shape[0])
-    #2) Determine initial calcs to be skipped based on weight-loss threshold
-    w_ = np.cumsum(abs(w))
-    w_ /= w_[-1]
-    skip = w_[w_ > thres].shape[0]
-    #3) Apply weights to values
-    df = {}
-    for name in series.columns:
-        seriesF, df_ = series[[name]].fillna(method='ffill').dropna(), pd.Series()
-        for iloc in range(skip, seriesF.shape[0]):
-            loc = seriesF.index[iloc]
-            if not np.isinf(series.loc[loc, name]): 
-                continue #exclude Na
-            df_[loc] = np.dot(W[-(iloc+1):, :].T, seriesF.loc[:loc])[0, 0]
-            k*(d-k+1)
-        df[name] = df_.copy(deep=True)
-    df = pd.concat(df, axis=1)
-    return df
-
-def getWeights_FFD(d, thres):
-    w, k = [1.], 1
-    while True:
-        w_ = -w[-1]/k*(d-k+1)
-        if(abs(w_) < thres): 
-            break
-        w.append(w_) 
-        k+=1
-    return np.array(w[::-1]).reshape(-1,1)
-#---------------------------------
-def fracDiff_FFD(series, d, thres=1e-5):
-    # Constant widht window (new solution)
-    w = getWeights_FFD(d, thres)
-    widht = len(w)-1
-    df = {}
-    for name in series.columns:
-        seriesF = series[[name]].fillna(method='ffill').dropna()
-        df_ = pd.Series()
-        for iloc1 in range(widht, seriesF.shape[0]):
-            loc0 = seriesF.index[iloc1 - widht]
-            loc1 = seriesF.index[iloc1]
-            #print(loc0, loc1, widht)
-            if(not np.isfinite(series.loc[loc1, name])):
-                continue # Exclude NaNs
-            #print('>> ',np.dot(w.T, seriesF.loc[loc0:loc1])[0, 0])
-            df_[loc1] = np.dot(w.T, seriesF.loc[loc0:loc1])[0, 0]
-        df[name] = df_.copy(deep=True)
-    df = pd.concat(df, axis=1)
-    return df
-#----------------------------
-def plotWeights_FFD(dRange, nPlots, size):
-    w = pd.DataFrame()
-    for d in np.linspace(dRange[0], dRange[1], nPlots):
-        w_ = getWeights_FFD(d, thres=0.01)
-        w_ = pd.DataFrame(w_, index=range(w_.shape[0])[::-1], columns=[d])
-        w = w.join(w_, how='outer')
-    ax = w.plot(figsize=(12, 8))
-    ax.legend(loc='lower right')
-    plt.show()
-    return
-
-
-
